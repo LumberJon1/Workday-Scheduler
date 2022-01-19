382e660da310462687036c68fd50c72cceaaa1b7
Jonathan W <jwilliams12117@gmail.com>
2022-01-18 23:20:30 -0700

    Added ability to edit task content and push to array

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -21,8 +21,39 @@ $(".container");
 //If the time is before an element, those future elements should be white
 
 //Define the storage array that will hold the elements' data
+tasks = []
 
 //On clicking the div, it should change to a textarea and allow the user to type content.
+var editContent = function() {
+    var text = $(this).text().trim();
+
+    var textInput = $("<textarea>")
+        .addClass("form-control col-10")
+        .val(text);
+
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+}
+//Listener for the onclick event within hourly task
+$(".container").on("click", ".hourly-task", editContent);
+
+//function that updates content and saves to the local array
+var updateContent = function() {
+    var content = $(this).parent("div").children("textarea").val();
+    var position = $(this).parent("div").children("span").text();
+    
+    //Append the values to the local array and save
+    tasks.push({position: position, content: content});
+    console.log(tasks);
+
+    //Change back to a <div> element from <textarea>
+    var originalDiv = $("<div>").text(content)
+        .addClass("col-10 hourly-task text-center border border-secondary rounded-sm mb-1 p-3 bg-light");
+    $(this).parent("div").children("textarea").replaceWith(originalDiv);
+}
+
+//Listener for the saving of edited content
+$(".btn-primary").on("click", updateContent);
 
 //When the focus changes, it should revert to a div, but keep the text content.
 
