74a1858798f80d69cba9523a0ceb6cf978b16d85
Jonathan W <jwilliams12117@gmail.com>
2022-01-22 12:54:02 -0700

    Added task persistence for hour blocks

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -8,44 +8,43 @@ var jumboDate = moment().format("dddd MMMM Do, YYYY");
 $("#currentDay").text(jumboDate);
 
 //Set the time in the left-hand span elements with moment.js so it can be evaluated
-$(".row").each(function(index) {
-    $(this).children("span").text();
 
-    var time = moment().set("hour", 9 + index);
-    console.log(time.hour());
+    /*The below function currently doesn't get called when the updateContent() function runs,
+    so I will need to wrap it in its own function name and call it probably within the
+    originalDiv declaration around line 80*/
+var formatHours = function() {
 
-    //Set the content of each div's span element to the "h:00" format of current time
-    var blockTime = moment().hour(9 + index).format("h:00");
-    $(this).children("span").text(blockTime);
+    $(".row").each(function(index) {
+        $(this).children("span").text();
 
-    //Define selector to style
-    var hourBlock = $(this).children("div");
-    console.log(hourBlock);
+        var time = moment().set("hour", 9 + index);
 
-    if (moment().isAfter(time)) {
-        console.log("This block is before the current time");
-        //Remove any previous color styles and add the grayed-out style
-        hourBlock.removeClass("bg-light bg-primary");
-        hourBlock.addClass("bg-secondary");
+        //Set the content of each div's span element to the "h:00" format of current time
+        var blockTime = moment().hour(9 + index).format("h:00");
+        $(this).children("span").text(blockTime);
 
-    }
-    else if (moment().isBefore(time)) {
-        console.log("This time block has not occurred yet.");
-        //Remove any previous color styles
-        hourBlock.removeClass("bg-primary bg-secondary");
-        hourBlock.addClass("bg-light");
-    }
-    else {
-        console.log("This is the current hour");
-        //Apply a light blue highlight or border
-        hourBlock.removeClass("bg-light bg-secondary");
-        hourBlock.addClass("bg-primary");
-    }
-});
+        //Define selector to style
+        var hourBlock = $(this).children("div");
 
+        if (moment().isAfter(time)) {
+            //Remove any previous color styles and add the grayed-out style
+            hourBlock.removeClass("bg-light bg-primary");
+            hourBlock.addClass("bg-secondary");
 
-//Define the storage array that will hold the elements' data
-tasks = []
+        }
+        else if (moment().isBefore(time)) {
+            //Remove any previous color styles
+            hourBlock.removeClass("bg-primary bg-secondary");
+            hourBlock.addClass("bg-light");
+        }
+        else {
+            //Apply a light blue highlight or border
+            hourBlock.removeClass("bg-light bg-secondary");
+            hourBlock.addClass("bg-primary");
+        }
+    });
+}
+var tasks;
 
 //On clicking the div, it should change to a textarea and allow the user to type content.
 var editContent = function() {
@@ -57,7 +56,41 @@ var editContent = function() {
 
     $(this).replaceWith(textInput);
     textInput.trigger("focus");
+
+    //Possibly add a function call on change, rather than just on clicking "update"
+
 }
+
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+};
+
+var loadTasks = function() {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    //If there is nothing in localStorage, a new array needs to be made
+    if (!tasks) {
+        tasks = {
+            hour9: "",
+            hour10: "",
+            hour11: "",
+            hour12: "",
+            hour1: "",
+            hour2: "",
+            hour3: "",
+            hour4: "",
+            hour5: ""
+        };
+    };
+
+    console.log("tasks:", tasks);
+
+    //Somehow validate tasks to only include one per time block
+    // for (var i = 0; i < tasks.length; i++) {
+    //     tasks[i] = loadedTasks[i];
+    // };
+};
+
 //Listener for the onclick event within hourly task
 $(".container").on("click", ".hourly-task", editContent);
 
@@ -69,16 +102,65 @@ var updateContent = function() {
     //Append the values to the local array and save
     /*This currently pushes and doesn't check if there is already an entry
     for that particular hour; this needs to be fixed somehow. */
-    tasks.push({position: position, content: content});
-    console.log(tasks);
+    loadTasks();
 
+    //Determine which hour to set the value to content within
+    console.log("position: "+position);
+    switch(position) {
+        case "9:00":
+            console.log("adding to position 9:00");
+            tasks.hour9 = content;
+            break;
+        case "10:00":
+            console.log("adding to position 10:00");
+            tasks.hour10 = content;
+            break;
+        case "11:00":
+            console.log("adding to position 11:00");
+            tasks.hour11 = content;
+            break;
+        case "12:00":
+            console.log("adding to position 12:00");
+            tasks.hour12 = content;
+            break;
+        case "1:00":
+            console.log("adding to position 1:00");
+            tasks.hour1 = content;
+            break;
+        case "2:00":
+            console.log("adding to position 2:00");
+            tasks.hour2 = content;
+            break;
+        case "3:00":
+            console.log("adding to position 3:00");
+            tasks.hour3 = content;
+            break;
+        case "4:00":
+            console.log("adding to position 4:00");
+            tasks.hour4 = content;
+            break;
+        case "5:00":
+            console.log("adding to position 5:00");
+            tasks.hour5 = content;
+            break;
+        default:
+            break;
+    }
+    
     //When the user clicks the button on the right, the data should be appended to localStorage.
+    saveTasks();
 
     //Change back to a <div> element from <textarea>
     var originalDiv = $("<div>").text(content)
         .addClass("col-10 hourly-task text-center border border-secondary rounded-sm mb-1 p-3 bg-light");
     $(this).parent("div").children("textarea").replaceWith(originalDiv);
+    
+    //re-format the colors and styles of each div
+    formatHours();
 }
 
 //Listener for the saving of edited content
 $(".btn-primary").on("click", updateContent);
+
+//Format the page on load
+formatHours();
